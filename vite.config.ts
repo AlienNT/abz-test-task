716b4b28d968adb079bf5156df767993a518dfa2
Trysten M <nick.tristen22@gmail.com>
2024-12-08 15:45:42 +0200

    update(vite.config) add base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === 'build' ? '/abz-test-task/' : '/';
+export default defineConfig(({ command, mode }) => {
+  const { VITE_IS_DEPLOYED_ON_GITHUB } = loadEnv(mode, process.cwd())
+  const base =
+    command === 'build' && VITE_IS_DEPLOYED_ON_GITHUB === 'true' ? '/abz-test-task/' : '/'
+
+  console.log({ base, mode, command })
+
   return {
     base,
     plugins: [vue()],
