0ff628c8cd03ec3099764bfb6292cdd0aef469af
Trysten M <nick.tristen22@gmail.com>
2024-12-09 20:05:30 +0200

    add(components/UI) add email component

---
+++ src/components/UI/VEmail.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface EmailPropsInterface {
+  value: string;
+  title?: string;
+  target?: '_blank' | '_self'
+}
+
+withDefaults(defineProps<EmailPropsInterface>(), {
+  target: '_self'
+})
+</script>
+
+<template>
+<div class="email">
+  <a
+    :target="target"
+    :href="`mailto:${value}`"
+  >
+    {{title || value}}
+  </a>
+</div>
+</template>
+
+<style scoped>
+
+</style>
