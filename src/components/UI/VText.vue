938cfb0a65f3b64d3787940226ab58d5360ffdba
Trysten M <nick.tristen22@gmail.com>
2024-12-09 20:05:48 +0200

    add(components/UI) add text component

---
+++ src/components/UI/VText.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+interface TextPropsInterface {
+  value: string
+}
+defineProps<TextPropsInterface>()
+</script>
+
+<template>
+<div class="text">{{value}}</div>
+</template>
+
+<style scoped>
+
+</style>
