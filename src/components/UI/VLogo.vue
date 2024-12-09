119d86a0179b0955afb1f976a9ede7a8eab361e2
Trysten M <nick.tristen22@gmail.com>
2024-12-09 20:05:36 +0200

    add(components/UI) add logo component

---
+++ src/components/UI/VLogo.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface LogoPropsInterface {
+  image: string
+  width?: number
+  height?: number
+  alt?: string
+}
+
+const props = withDefaults(defineProps<LogoPropsInterface>(), {
+  alt: 'logo image',
+})
+
+const styles = computed(() => [
+  props.height ? `height: ${props.height}px` : '',
+  props.height ? `width: ${props.width}px` : '',
+].join('; '))
+</script>
+
+<template>
+  <div
+    class="logo"
+    :style="styles"
+  >
+    <img
+      class="logo__image"
+      :src="image"
+      :alt="alt"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.logo {
+  display: flex;
+}
+.logo__image {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+</style>
