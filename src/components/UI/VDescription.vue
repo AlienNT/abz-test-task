97a088697e6f99728079de29f6477919622ece14
Trysten M <nick.tristen22@gmail.com>
2024-12-09 20:05:24 +0200

    add(components/UI) add description component

---
+++ src/components/UI/VDescription.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface DescriptionPropsInterface {
+  value: string
+  color?: string
+  fonsSize?: string
+}
+
+const props = defineProps<DescriptionPropsInterface>()
+
+const styles = computed(() => [
+  props.color ? `color: ${props.color}` : '',
+  props.fonsSize ? `font-size: ${props.fonsSize}` : '',
+].join('; '))
+</script>
+
+<template>
+  <div class="description" :style="styles">
+    {{ value }}
+  </div>
+</template>
+
+<style scoped lang="scss">
+.description {
+  font-size: 16px;
+  text-align: center;
+  line-height: 26px;
+}
+</style>
