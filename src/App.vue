146ad018af2e150edfbd050b61a2f1f32d62c922
Trysten M <nick.tristen22@gmail.com>
2024-12-09 20:14:24 +0200

    update(appComponent) set viewport size after mounted

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
 import TheHeader from '@/components/TheHeader.vue'
 import TheMain from '@/components/TheMain.vue'
+import { onMounted, onUnmounted } from 'vue'
+import { getViewportSize, setStyleVariable } from '@/helpers/methodsHelper.ts'
+
+function setViewportSize() {
+  const { width, height } = getViewportSize()
+
+  setStyleVariable('width', `${width}px`)
+  setStyleVariable('height', `${height}px`)
+}
+
+onMounted(() => {
+  setViewportSize()
+  // addEventListener('resize', setViewportSize)
+})
+
+onUnmounted(() => {
+  // removeEventListener('resize', setViewportSize)
+})
 </script>
 
 <template>
