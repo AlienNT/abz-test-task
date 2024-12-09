66f959860a9cfa740e9d011aa97a377e83744920
Trysten M <nick.tristen22@gmail.com>
2024-12-09 20:05:42 +0200

    add(components/UI) add phone component

---
+++ src/components/UI/VPhone.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface PhonePropsInterface {
+  value: string;
+  title?: string;
+  target?: '_blank' | '_self'
+}
+
+withDefaults(defineProps<PhonePropsInterface>(), {
+  target: '_self'
+})
+</script>
+
+<template>
+<div class="phone">
+  <a
+    :href="`tel:${value}`"
+    :target="target"
+  >
+    {{title || value}}
+  </a>
+</div>
+</template>
+
+<style scoped>
+
+</style>
