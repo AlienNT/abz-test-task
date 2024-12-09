8904833f9a353cc3109509cdb3b6a4daa71afde0
Trysten M <nick.tristen22@gmail.com>
2024-12-09 20:11:19 +0200

    add(helpers) add images helper

---
+++ src/helpers/imagesPath.ts
@@ -0,0 +1,7 @@
+const path = import.meta.env.BASE_URL
+
+export default {
+  LOGO: path + 'images/Logo.svg',
+  USER_PHOTO: path + 'images/photo-cover.svg',
+  SUCCESS_IMAGE: path + 'images/success-image.svg',
+}
