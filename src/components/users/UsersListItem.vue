557aa9ebc33fc7bc0ba0a6c477cd737cdc603235
Trysten M <nick.tristen22@gmail.com>
2024-12-09 20:06:39 +0200

    add(components/Users) add user list item component

---
+++ src/components/users/UsersListItem.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import UserAvatar from '@/components/users/UserAvatar.vue'
+import UserField from '@/components/users/UserField.vue'
+import type { UserInterface } from '@/types/user.ts'
+
+interface UserPropsInterface {
+  userData: UserInterface
+}
+
+defineProps<UserPropsInterface>()
+</script>
+
+<template>
+  <div class="user-list-item">
+    <div class="user-fields">
+      <UserAvatar
+        class="user-fields__avatar"
+        :image="userData.photo"
+        :alt="userData.name + ' photo'"
+      />
+      <UserField class="user-fields__name" :value="userData.name" type="text" />
+      <UserField class="user-fields__position" :value="userData.position" type="text" />
+      <UserField class="user-fields__email" :value="userData.email" type="email" />
+      <UserField class="user-fields__phone" :value="userData.phone" type="phone" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.user-list-item {
+  max-width: 370px;
+}
+
+.user-fields {
+  padding: 20px;
+  border-radius: 10px;
+  background: rgb(255, 255, 255);
+  height: 100%;
+  text-align: center;
+}
+
+.user-fields__name,
+.user-fields__avatar {
+  margin-bottom: 20px;
+}
+
+.user-fields__avatar {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+</style>
