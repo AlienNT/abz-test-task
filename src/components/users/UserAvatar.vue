7d0191d08bd07814a98f7cfef5c80537d07ab397
Trysten M <nick.tristen22@gmail.com>
2024-12-09 20:06:16 +0200

    add(components/Users) add user avatar component

---
+++ src/components/users/UserAvatar.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+interface UserAvatarPropsInterface {
+  image: string
+  alt: string
+  width?: number
+  height?: number
+}
+
+defineProps<UserAvatarPropsInterface>()
+</script>
+
+<template>
+  <div class="user-avatar">
+    <div class="user-avatar__wrapper">
+      <img class="user-avatar__img" :src="image" :alt="alt" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.user-avatar {
+  width: 70px;
+  height: 70px;
+}
+
+.user-avatar__wrapper {
+  position: relative;
+  width: 0;
+  height: 0;
+  padding-top: 100%;
+  padding-left: 100%;
+  display: flex;
+}
+
+.user-avatar__img {
+  top: 0;
+  left: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 50%;
+}
+</style>
