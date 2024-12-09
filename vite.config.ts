9ea1096c2ba9984697b889be82f2098a1a4208fa
Trysten M <nick.tristen22@gmail.com>
2024-12-09 19:57:21 +0200

    update(vite.config) add autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -19,5 +20,12 @@ export default defineConfig(({ command, mode }) => {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
+    css: {
+      postcss: {
+        plugins: [
+          autoprefixer({})
+        ]
+      }
+    }
   }
 })
