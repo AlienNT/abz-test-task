11f65ddcd713c045c2f5c233b2c8b5072b53d3c6
Trysten M <nick.tristen22@gmail.com>
2024-12-09 20:05:54 +0200

    add(components/UI) add title component

---
+++ src/components/UI/VTitle.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface TitlePropsInterface {
+  value: string
+  color?: string
+  fontSize?: string
+  tagName?: string
+}
+
+const props = withDefaults(defineProps<TitlePropsInterface>(), {
+  tagName: 'div',
+})
+
+const styles = computed(() => [
+  props.color ? `color: ${props.color}` : '',
+].join('; '))
+</script>
+
+<template>
+ <component
+   :is="tagName"
+   :style="styles"
+   class="title"
+ >
+   {{value}}
+ </component>
+</template>
+
+<style scoped>
+.title {
+  width: 100%;
+  font-size: 40px;
+  text-align: center;
+  line-height: 40px;
+}
+</style>
